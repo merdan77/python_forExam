23c4f5850f94f7253839328af3804ea684c28ba3
merdan77 <tagandurdyyewmerdan005@gmail.com>
2025-01-16 19:15:24 +0300

    praktic01

---
+++ 15_lesson/praktik01.py
@@ -0,0 +1,10 @@
+ishgarler = {"Mekan":{"hunari":"programist",
+                      "bilimi" : "orta",
+                      "yashy":22},
+"Oraz" : {"hunari":"hasapchy",
+          "bilimi":"yokary",
+          "yashy":30}
+}
+print(ishgarler["Mekan"])
+print(ishgarler["Mekan"]["hunari"])
+print(ishgarler["Mekan"]["bilimi"])
