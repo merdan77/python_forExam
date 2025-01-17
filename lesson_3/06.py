46402aa68a9e9c04ab2cd8e98e2a291e685a5e82
merdan77 <tagandurdyyewmerdan005@gmail.com>
2025-01-17 17:29:33 +0300

    praktik

---
+++ lesson_3/06.py
@@ -0,0 +1,12 @@
+is_guni = int(input("Islenen gunlerin sany: "))
+cyk = int(input("Oyden ishe cykdayjy:"))
+cyk2 = int(input("Ishden oye cykdayjy:"))
+aycyk = is_guni * (cyk + cyk2)
+print(30 * "-")
+
+print(is_guni)
+print(cyk)
+print(cyk2)
+
+print(30 * "-")
+print(aycyk)
