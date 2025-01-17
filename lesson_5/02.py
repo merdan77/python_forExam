0531ce4088d5c1da9f85aaed2ecfaf5bdbaa2145
merdan77 <tagandurdyyewmerdan005@gmail.com>
2025-01-17 17:36:12 +0300

    praktik

---
+++ lesson_5/02.py
@@ -0,0 +1,10 @@
+username = input("Username: ")
+if username == "Resul":
+    print("Correct username")
+    password = input("Password: ")
+    if password == "123456":
+        print("Welcome to the program")
+    else:
+        print("Incorrect password")
+else:
+    print("Incorrect username")
