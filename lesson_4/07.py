7c38ccebf283a64fe8af22e332d7c6ea93b7901d
merdan77 <tagandurdyyewmerdan005@gmail.com>
2025-01-17 17:34:36 +0300

    praktik

---
+++ lesson_4/07.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+if num1 < num2:
+    print(f"Min number {num1}")
+elif num1 > num2:
+    print(f"Min number {num2}")
