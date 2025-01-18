fb3482c0cdb5db9419a46c53ca34b5a3c540a4c3
merdan77 <tagandurdyyewmerdan005@gmail.com>
2025-01-18 19:29:18 +0300

    praktika

---
+++ 16_lesson/05.py
@@ -0,0 +1,8 @@
+def hasaba_durmak(a,f,t)
+    print("Ady:",a)
+    print("Familyasy:",f)
+    print("Talyp belgisi:",t)
+    print("-")
+
+hasaba_durmak("Serdar","Meretliyew","194216")
+hasaba_durmak("Eziz","Nazarow","194210")
