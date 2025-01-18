50173ba94b8586ed90e11e3c6828bdd70b929da4
merdan77 <tagandurdyyewmerdan005@gmail.com>
2025-01-18 13:55:49 +0300

    praktik

---
+++ lesson_12/02.py
@@ -0,0 +1,17 @@
+soz = 'Knowledge'
+print(soz.upper())
+
+soz = 'Knowledge'
+print(soz.lower())
+
+soz = 'Knowledge'
+print(soz.swapcase())
+
+soz = 'Knowledge'
+print(soz.replace("e", "E"))
+
+soz = 'Knowledge'
+print(soz.index('e'))
+
+soz = 'Knowledge'
+print(soz.count('e'))
